c96a188b3d69a0272042401b89a077c639d1eafe
BulelaniKote <bulelanikote18@gmail.com>
2025-09-01 21:50:18 +0200

    making change

--- Testing/run_istanbul_analysis.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Istanbul Sales Analysis Dashboard...
-echo.
-echo This will open the Streamlit app in your default browser.
-echo.
-echo Press Ctrl+C to stop the server when you're done.
-echo.
-streamlit run streamlit_app.py
-pause
